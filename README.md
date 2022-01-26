590c850551543a61e177ffdb041bb09208fda55a
raymondjestrada <87677207+raymondjestrada@users.noreply.github.com>
2022-01-26 01:02:57 -0800

    changes

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# homework-week-9
+# README-Generator 
+https://github.com/raymondjestrada/homework-week-9-readme
+## Description
+This app allows developers to create a professional README.md file in a way that is clean, efficient, and easy to use. 
+## Table of Contents 
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+## Installation
+The following necessary dependencies must be installed to run the application properly: inquire, fs, util
+## Usage
+In order to use this app, run the app in the terminal/gitbash
+## License
+This project is licensed under the MIT license. 
+![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)
+## Contributing
+​Contributors: https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide
+## Tests
+The following is needed to run the test: node index.js
+## Questions
+If you have any questions about the repo, open an issue or contact raymondjestrada directly at : https://github.com/raymondjestrada
--- index.js
+++ index.js
@@ -1,15 +1,83 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require('fs');
+const util = require("util");
+
+const generatorMarkdown = require('./util/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: "input",
+    message: "What is the title of the project?",
+    name: "Title"
+}, {
+    type: "input",
+    message: "What is the project about? Give a detailed description of your project?",
+    name: "Description"
+}, {
+    type: "input",
+    message: "Table of Contents.",
+    name: "Table of Contents"
+}, {
+    type: "input",
+    message: "What does the user need to install to run this app (ie...dependencies)?",
+    name: "Installation"
+}, {
+    type: "input",
+    message: "How is the app used? Give instructions",
+    name: "Usage"
+}, {
+    type: "input",
+    message: "What liscence is being used? (ie...MIT)",
+    name: "License"
+}, {
+    type: "input",
+    message: "Who contributed to this project?:",
+    name: "Contributing"
+}, {
+    type: "input",
+    message: "What commands are needed to test this app?",
+    name: "Tests"
+}, {
+    type: "input",
+    message: "Contact info for inquiries.",
+    name: "Questions"
+}, {
+    type: 'input',
+    message: 'What is your Github username?',
+    name: 'Username'
+}, {
+    type: 'input',
+    message: 'What is your email address?',
+    name: 'Email'
+},
 
-
+]
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+    fs.writeFile(fileName, data, function(err) {
+        console.log(fileName)
+        console.log(data)
+        if (err) {
+            return console.log(err)
+        } else {
+            console.log("success")
+        }
+    })
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+        .then(function(data) {
+            writeToFile("README.md", generatorMarkdown(data));
+            console.log(data)
 
+        })
+
+}
 // Function call to initialize app
 init();
